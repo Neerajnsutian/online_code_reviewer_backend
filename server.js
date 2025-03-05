63059850702574608adc2a23d0c5ebd229d033c2
Neeraj Chaudhary <neerajofficial1919@gmail.com>
2025-03-05 11:13:26 +0530

    second commit

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const app = require('./src/app');
+const port = process.env.PORT || 8080;
 
 app.listen(3000, () => {
   console.log('Server is running on http://localhost/3000');
